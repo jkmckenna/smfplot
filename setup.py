9aa0cd964021eaf7ba7ea569e64d4ddb68b6f889
jkmckenna <jkmckenna@berkeley.edu>
2024-07-05 15:26:54 -0700

    New v2 notebook
    Added HMM modeling and more methylation analyses

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='smfplot',
       version='0.1.0',
+      packages=find_packages(),
       description='A python package to visualize single locus single molecule footprinting data.',
       url='https://github.com/jkmckenna/smfplot',
       author="Joseph McKenna",
